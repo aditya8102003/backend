879b1d598332c4b4c70015dc8a36ddde377c7698
aditya8102003 <111857199+aditya8102003@users.noreply.github.com>
2022-08-25 11:00:30 +0530

    EurekaServer microservice

---
+++ EurekaServer/src/test/java/com/fbs/eserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbs.eserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
